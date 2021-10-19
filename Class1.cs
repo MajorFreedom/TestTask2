6d6ba5e28969a026c45378b3059ea04576f8c326
Airat Galeev <76250570+MajorFreedom@users.noreply.github.com>
2021-10-19 12:45:46 +0300

    Add files via upload
    Class1 - код dll сборки

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnalyzerLibrary
+{
+    class ListMaker
+    {
+        private Dictionary<string, int> MakingWordList(StringBuilder sb)
+        {
+            Console.WriteLine("Making the word frequency list");
+            var wordsArray = sb.ToString().
+                Split(' ', ',', '.', '-', '!', '?', ':', ';', '"', '[', ']', '(', ')', '\r', '\t', '\n');
+
+            Dictionary<string, int> dictionaryWords = new Dictionary<string, int>();
+
+            foreach (var item in wordsArray)
+            {
+                if (dictionaryWords.ContainsKey(item))
+                {
+                    dictionaryWords[item]++;
+                }
+                else
+                {
+                    dictionaryWords.Add(item, 1);
+                }
+            }
+            Dictionary<string, int> sortedWords = dictionaryWords.OrderByDescending(w => w.Value).ToDictionary(w => w.Key, w => w.Value);
+            Console.WriteLine("Completed");
+            return sortedWords;
+        }
+
+    }
+}
